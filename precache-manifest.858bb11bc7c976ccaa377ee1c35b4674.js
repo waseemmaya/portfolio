7e63035389c099abe33ad2f48a02650f94b71db2
Waseem Maya <waseemmayaa@gmail.com>
2020-06-08 11:16:15 +0500

    Updates

---
+++ precache-manifest.858bb11bc7c976ccaa377ee1c35b4674.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bdd68bfcf7f2f07f1d5397d794bf9ef",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4fc3d393f7775672d230",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0535acca26546b0ab41a",
+    "url": "/portfolio/static/js/2.3fa6073c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.3fa6073c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fc3d393f7775672d230",
+    "url": "/portfolio/static/js/main.27ca7d4f.chunk.js"
+  },
+  {
+    "revision": "3ca375981f9b833547c1",
+    "url": "/portfolio/static/js/runtime-main.0ba2c954.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
